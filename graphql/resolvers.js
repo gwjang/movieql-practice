e98de166dd49647e0acc1b35222ecbf8a841a1fb
gwjang <zmdlgl@gmail.com>
2022-04-11 00:04:05 +0900

    graphql 연습 끝

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,10 @@
-import {getMovies} from "./db.js";
+import {getMovie, getMovies, getSuggestions} from "./db.js";
 
 const resolvers = {
     Query: {
         movies: (_, {limit, rating}) => getMovies(limit, rating),
+        movie: (_, {id}) => getMovie(id),
+        suggestions: (_, { id }) => getSuggestions(id)
     },
 };
 
